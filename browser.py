b7087e87daf363f9585830b590367c110be283e4
Sanvyx <67471083+sanvyx-sancre@users.noreply.github.com>
2025-01-24 23:16:16 +0300

    Color scheme change-More optimized

--- browser.py
+++ browser.py
@@ -2,20 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
-import re
 from urllib.parse import urlparse
-from PyQt5.QtWebEngineWidgets import QWebEnginePage
-
-class WebEnginePage(QWebEnginePage):
-    def __init__(self, parent=None):
-        super(WebEnginePage, self).__init__(parent)
-
-    def triggerAction(self, action, checked=False):
-        # Check if the action is fullscreen
-        if action == QWebEnginePage.FullScreen:
-            self.view().setWindowState(Qt.WindowFullScreen)
-        else:
-            super(WebEnginePage, self).triggerAction(action, checked)
 
 class Zenith(QMainWindow):
     def __init__(self):
@@ -34,62 +21,60 @@ class Zenith(QMainWindow):
         self.browser.load(QUrl('http://duckduckgo.com'))
         self.tabs.addTab(self.browser, 'Home')
 
+        self.back_shortcut = QShortcut(QKeySequence(Qt.MouseButton.BackButton), self)
+        self.back_shortcut.activated.connect(self.browser.back)
 
+        self.forward_shortcut = QShortcut(QKeySequence(Qt.MouseButton.ForwardButton), self)
+        self.forward_shortcut.activated.connect(self.browser.forward)
 
         navbar = QToolBar()
         navbar.setStyleSheet(self.get_navbar_styles())
         self.addToolBar(navbar)
 
-        back_btn = QAction('Back', self)
+        back_btn = QAction('⮜', self)
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        forward_btn = QAction('Forward', self)
+        forward_btn = QAction('⮞', self)
         forward_btn.triggered.connect(self.browser.forward)
         navbar.addAction(forward_btn)
 
-        search_btn = QAction('Search', self)
-        search_btn.triggered.connect(self.search)
-        navbar.addAction(search_btn)
+        refresh_btn = QAction('⭯', self)
+        refresh_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(refresh_btn)
+
+        home_btn = QAction('🏠', self)
+        home_btn.triggered.connect(self.home)
+        navbar.addAction(home_btn)
 
         self.url_bar = QLineEdit()
         self.url_bar.setStyleSheet(self.get_url_bar_styles())
         navbar.addWidget(self.url_bar)
-
         self.browser.urlChanged.connect(self.update_url)
-
         self.url_bar.returnPressed.connect(self.search)
 
-        refresh_btn = QAction('Refresh', self)
-        refresh_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(refresh_btn)
+        search_btn = QAction('🔍', self)
+        search_btn.triggered.connect(self.search)
+        navbar.addAction(search_btn)
 
-        home_btn = QAction('Home', self)
-        home_btn.triggered.connect(self.home)
-        navbar.addAction(home_btn)
-
-        self.tabs.currentChanged.connect(self.change_tab)
-
-        addtab_btn = QAction('New Tab', self)
-        addtab_btn.triggered.connect(self.add_new_tab)
+        addtab_btn = QAction('+', self)
         navbar.addAction(addtab_btn)
+        addtab_btn.triggered.connect(lambda: self.add_new_tab())
+
 
     def close_current_tab(self, index):
         self.tabs.removeTab(index)
 
-    def change_tab(self, index):
-        if index >= 0:
-            self.browser = self.tabs.currentWidget()
-
-    def add_new_tab(self, qurl=None):
-        if qurl is None:
-            qurl = QUrl('http://duckduckgo.com')
+    def add_new_tab(self, qurl=QUrl('http://duckduckgo.com')):
         browser = QWebEngineView()
         browser.setUrl(qurl)
-        i = self.tabs.addTab(browser, qurl.host())
-        self.tabs.setCurrentIndex(i)
         browser.urlChanged.connect(self.update_url)
-        browser.loadFinished.connect(lambda _, i=i: self.tabs.setTabIcon(i, browser.icon()))
+
+    # Add a new tab with the browser widget
+        index = self.tabs.addTab(browser, "New Tab")
+        self.tabs.setCurrentIndex(index)
+
+
 
     def home(self):
         self.browser.setUrl(QUrl('http://duckduckgo.com'))
@@ -114,43 +99,69 @@ class Zenith(QMainWindow):
         self.url_bar.setText(q.toString())
         index = self.tabs.currentIndex()
         parsed_url = urlparse(q.toString())
-        self.tabs.setTabText(index, parsed_url.netloc)
-        self.tabs.setTabIcon(index, self.browser.icon())
+        self.tabs.setTabText(index, parsed_url.netloc or "New Tab")
 
 
-    # Stylesheets for the different components
     def get_styles(self):
         return """
         QMainWindow {
-            background-color: #1E1E1E;
-            color: #D3D3D3;
-            font-family: Arial, sans-serif;
+            background-color: #1E1E28;
+            color: #CDD6F4;
+            font-family: 'Segoe UI', Arial, sans-serif;
         }
         """
 
     def get_navbar_styles(self):
         return """
         QToolBar {
-            background-color: #333;
+            background-color: #181825;
             border: none;
             padding: 5px;
         }
         QToolButton {
-            color: #FFF;
-            background: #555;
+            color: #F5C2E7;
+            background: #313244;
             border-radius: 5px;
-            padding: 8px;
-            margin: 3px;
+            padding: 6px;
+            margin: 2px;
+            font-size: 14px;
         }
         QToolButton:hover {
-            background: #777;
+            background: #45475A;
         }
         QLineEdit {
-            background-color: #555;
-            color: white;
-            padding: 6px;
+            background-color: #313244;
+            color: #CDD6F4;
+            padding: 5px;
             border-radius: 5px;
             font-size: 14px;
+        }
+        QLineEdit:focus {
+            background-color: #45475A;
+            border: 1px solid #89B4FA;
+        }
+        """
+
+    def get_tab_styles(self):
+        return """
+        QTabWidget::pane {
+            border: 1px solid #313244;
+            background-color: #1E1E28;
+            border-radius: 5px;
+        }
+        QTabBar::tab {
+            background: #313244;
+            color: #CDD6F4;
+            padding: 8px;
+            border-radius: 5px;
+            margin: 2px;
+        }
+        QTabBar::tab:selected {
+            background: #89B4FA;
+            color: #1E1E28;
+        }
+        QTabBar::tab:hover {
+            background: #45475A;
         }
         """
 
@@ -160,36 +171,13 @@ class Zenith(QMainWindow):
             border: none;
             border-radius: 5px;
             padding: 5px;
-            background-color: #444;
-            color: white;
+            background-color: #313244;
+            color: #CDD6F4;
             font-size: 14px;
         }
         QLineEdit:focus {
-            background-color: #333;
-            border: 1px solid #4A90E2;
-        }
-        """
-
-    def get_tab_styles(self):
-        return """
-        QTabWidget::pane {
-            border: 1px solid #444;
-            background-color: #333;
-            border-radius: 5px;
-        }
-        QTabBar::tab {
-            background: #555;
-            color: white;
-            padding: 10px;
-            font-size: 14px;
-            border-radius: 5px;
-        }
-        QTabBar::tab:selected {
-            background: #4A90E2;
-            color: white;
-        }
-        QTabBar::tab:hover {
-            background: #777;
+            background-color: #45475A;
+            border: 1px solid #89B4FA;
         }
         """
 
